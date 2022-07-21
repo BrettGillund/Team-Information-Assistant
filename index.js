7cabf838d8ef16d63e4eb448eafecf4da02232e8
Brett Gillund <brettgillund2@gmail.com>
2022-07-21 18:33:03 -0500

    intial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// Things that need to be included in this command line applicatoin:
+
+// 1. List prompt with selectable team members.
+
+// 2. When chosen an HTML file is created that desplays a nicely formated team roser based on user input.
+
+// 3. When an email address is clicked in the HTML default email application is opened and the email populates the TO field with team members inforamtion. 
+
+// 4. When a githib address is clicked in the HTML default team members github is opened in a new tab. 
+
+// 5. when application is launched user is promped to provide team manager's NAME, EMPLOTEE ID, EMAIL ADDRESS, AND OFFICE NUMBER.
+
+// 6. when entered user is presented with a menu with options to add an engineer or an intern or to finish building team.
+
+// 7. when engineer is selected user is prompted to enter the engineer’s name, ID, email, and GitHub username, and then is redirected to the main menu.
+
+// 8. when intern is selected user is prompted to enter the intern’s name, ID, email, and school, and then is redirected to the main menu.
+
+// 9. when finish building my team is selected exit the application and the HTML is generated.
+
